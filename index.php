0f167df9a1f7fa345470e600e0d79fe48de67a4a
Pavel <p.savlevic@gmail.com>
2019-06-26 14:14:17 +0300

    is php arrejaus generuojam input forma html'e

--- index.php
+++ index.php
@@ -1,21 +1,23 @@
 <?php
 
-function alaus() {
-    return print "salto alaus bokalas,";
-}
-
-function duona(){
-    return print "kepta duona su suriu";
-}
-
-$array = [
-        "alaus",
-        "duona"
+$form = [
+    'action' => 'index.php',
+    'method' => 'POST',
+    'fields' => [
+        'first_name' => [
+            'label' => 'Vardas:',
+            'type' => 'text',
+            'value' => '',
+            'placeholder' => 'Onute'
+        ],
+        'last_name' => [
+            'label' => 'Pavarde:',
+            'type' => 'text',
+            'value' => '',
+            'placeholder' => 'Kimarinskiene'
+        ],
+    ]
 ];
-
-foreach ($array as $funkcijos_pavadinimas){
-    $funkcijos_pavadinimas();
-}
 
 
 ?>
@@ -29,7 +31,17 @@ foreach ($array as $funkcijos_pavadinimas){
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-
+<form action="/<?php print $form['action']; ?>" method="<?php print $form['method']; ?>">
+    <label>
+        <?php foreach ($form['fields'] as $field_id => $field): ?>
+            <?php print $field['label']; ?> <br>
+            <input type="<?php print $field['type']; ?>" value="<?php print $field['value']; ?>"
+                   placeholder="<?php print $field['placeholder']; ?>" name="<?php print $field_id; ?>"><br>
+        <?php endforeach; ?>
+    </label>
+    <br> <br>
+    <button name="action" value="save">Save</button>
+</form>
 
 </body>
 </html>
