973352a8b6626041ddc3b31739077eadada22324
Pavel <p.savlevic@gmail.com>
2019-07-03 10:05:00 +0300

    FULL (Manto) filtravimas+validatinimas+json save array to file, json decode file to array

--- functions/file.php
+++ functions/file.php
@@ -1,45 +1,34 @@
 <?php
+
 /**
- * Issaugo arreju i faila
- * @param array $array
- * @param string $file
+ * Saves array to file
+ * @param type array. $array - array we want to convert
+ * @param type file. $file_name - file into where we want to save
  * @return boolean
  */
-function array_to_file($array, $file) {
-    $encoded_array = json_encode($array);
-    $success = file_put_contents($file, $encoded_array);
-    if($success !== false) {
+function array_to_file($array, $file_name) {
+    $array_encode_to_jason_string = json_encode($array);
+    $success = file_put_contents($file_name, $array_encode_to_jason_string); //$success atiduoda irasyta baitu skaiciu arba false
+    if ($success !== FALSE) {
         return true;
     } else {
         return false;
     }
 }
 
-
 /**
  * Decoding array from file
- * @param string $filename
- * @return bool|mixed
+ * @param type file. $file_name - file we get data from
+ * @return array|boolean
  */
-//function file_to_array ($filename) {
-//    if(file_exists($filename)) {
-//        $encoded_string = file_get_contents($filename);
-//        if ($encoded_string !== false) {
-//            return json_decode($encoded_string, true);
-//        }
-//    }
-//        return false;
-//}
-
-
-function file_to_array($file) {
-    if (file_exists($file)) {
-        $encoded_string = file_get_contents($file);
+function file_to_array($file_name) {
+    if (file_exists($file_name)) {
+        $encoded_string = file_get_contents($file_name);
         if ($encoded_string !== false) {
             return json_decode($encoded_string, true);
+        } else {
+            return false;
         }
     }
-    return false;
 }
-
-?>
+ ?>
--- index.php
+++ index.php
@@ -1,71 +1,33 @@
 <?php
+
+//setcookie("COOKIE_NAME", "COOKIE_VALUE", time() + 500, "/");
+//
+//var_dump($_COOKIE);
+
+
 require 'functions/file.php';
-$form = [
-    'action' => 'index.php',
-    'method' => 'POST',
-    'fields' => [
-        'first_name' => [
-            'label' => 'Vardas:',
-            'type' => 'text',
-            'value' => '',
-            'placeholder' => '',
-            'validator' => [
-                'validate_not_empty'
-            ]
-        ],
-        'last_name' => [
-            'label' => 'Pavarde:',
-            'type' => 'text',
-            'value' => '',
-            'placeholder' => '',
-            'validator' => [
-                'validate_not_empty'
-            ]
-        ],
-        'age' => [
-            'label' => 'amzius:',
-            'type' => 'text',
-            'value' => '',
-            'placeholder' => '',
-            'validator' => [
-                'validate_not_empty',
-                'validate_is_number',
-                'validate_is_positive',
-                'validate_max_100',
-            ]
-        ]
-    ],
-    'callbacks' => [
-        'success' => 'form_success',
-        'fail' => 'form_fail'
-    ]
-];
-//filtered
-function get_form_input($form)
-{
-    $parameters = [];
+
+function get_form_input($form) {
+    $filter_parameters = [];
+
     foreach ($form['fields'] as $field_id => $field) {
-        $parameters[$field_id] = FILTER_SANITIZE_SPECIAL_CHARS;
+        if (isset($field['filter'])) {
+            $filter_parameters[$field_id] = $field['filter'];
+        } else {
+            $filter_parameters[$field_id] = FILTER_SANITIZE_SPECIAL_CHARS;
+        }
     }
-    return filter_input_array(INPUT_POST, $parameters);
+
+    return filter_input_array(INPUT_POST, $filter_parameters);
 }
 
-$safe_input = get_form_input($form);
-validate_form($safe_input, $form);
-
-if ($safe_input) {
-    validate_form($safe_input, $form);
-}
-
-function validate_form($safe_input, &$form)
-{
+function validate_form($filtered_input, &$form) {
     $success = true;
     foreach ($form['fields'] as $field_id => &$field) {
-        $field_value = $safe_input[$field_id];
-        $field['value'] = $field_value;
-        if (isset($field['validator'])) {
-            foreach ($field['validator'] as $validator) {
-                $is_valid = $validator($safe_input[$field_id], $field);
+        $field['value'] = $filtered_input[$field_id];
+        if (isset($field['validators'])) {
+            foreach ($field['validators'] as $validator) {
+                $is_valid = $validator($filtered_input[$field_id], $field);
                 if (!$is_valid) {
                     $success = false;
                     break;
@@ -73,116 +35,187 @@ function validate_form($safe_input, &$form)
             }
         }
     }
+
     if ($success) {
         if (isset($form['callbacks']['success'])) {
-            $form['callbacks']['success']($safe_input, $form);
+            $form['callbacks']['success']($filtered_input, $form);
         }
     } else {
         if (isset($form['callbacks']['fail'])) {
-            $form['callbacks']['fail']($safe_input, $form);
+            $form['callbacks']['fail']($filtered_input, $form);
         }
     }
+
     return $success;
 }
 
-function form_success($safe_input, &$form)
-{
+function form_success($filtered_input, &$form) {
     $new_data = [];
     $old_data = file_to_array('info.txt');
-
     if (!empty($old_data)) {
         $new_data = $old_data;
     }
-
-    $new_data[] = $safe_input;
+    $new_data[] = $filtered_input;
     array_to_file($new_data, 'info.txt');
 }
 
-function form_fail($safe_input, &$form)
-{
-    print 'blogai';
+//Kitas budas:
+//function form_success($filtered_input, &$form) {
+//    $encoded_filtered_input = json_encode($filtered_input) . "\r\n";
+//    file_put_contents('info.txt', $encoded_filtered_input, FILE_APPEND);
+//}
+
+function form_fail($filtered_input, &$form) {
+    print 'Nepavyko įrašyti informacijos į failą.';
 }
 
-function validate_not_empty($safe_input, &$field)
-{
-    if (strlen($safe_input) == 0) {
-        $field['error'] = 'laukelis tuscias';
+function validate_not_empty($field_input, &$field) {
+    if (strlen($field_input) == 0) {
+        $field['error'] = 'Laukas tuščias';
     } else {
         return true;
     }
 }
 
-function validate_is_number($safe_input, &$field)
-{
-    if (!is_numeric($safe_input)) {
-        $field['error'] = 'iveskit skaiciu';
+function validate_is_number($field_input, &$field) {
+    if (!is_numeric($field_input)) {
+        $field['error'] = 'Įveskite skaičių!';
     } else {
         return true;
     }
 }
 
-function validate_is_positive($field_input, &$field)
-{
-    if ($field_input <= 0) {
-        $field['error'] = 'Iveskite teigiama skaiciu';
+function validate_is_positive($field_input, &$field) {
+    if ($field_input < 0) {
+        $field['error'] = 'Įveskite teigiamą skaičių.';
     } else {
         return true;
     }
 }
 
-function validate_max_100($field_input, &$field)
-{
+function validate_max_100($field_input, &$field) {
     if ($field_input > 100) {
-        $field['error'] = 'Iveskite skaiciu iki 100';
+        $field['error'] = 'Tiek žmonės negyvena.';
     } else {
         return true;
     }
+}
+
+$form = [
+    'action' => 'index.php',
+    'method' => 'POST',
+    'fields' => [
+        'first_name' => [
+            'label' => 'Vardas',
+            'type' => 'text',
+            'value' => '',
+            'validators' => [
+                'validate_not_empty',
+            ],
+            'placeholder' => 'Įveskite vardą.'
+        ],
+        'second_name' => [
+            'label' => 'Pavardė',
+            'type' => 'text',
+            'validators' => [
+                'validate_not_empty',
+            ],
+            'value' => '',
+            'placeholder' => 'Įveskite pavardę.'
+        ],
+        'third_name' => [
+            'label' => 'Pravardė',
+            'type' => 'text',
+            'validators' => [
+                'validate_not_empty',
+            ],
+            'value' => '',
+            'placeholder' => 'Įveskite pravardę.'
+        ],
+        'age' => [
+            'label' => 'Amžius',
+            'type' => 'text',
+            'validators' => [
+                'validate_not_empty',
+                'validate_is_number',
+                'validate_is_positive',
+                'validate_max_100'
+            ],
+            'value' => '',
+            'placeholder' => 'Įveskite amžių.'
+        ],
+        'digit' => [
+            'label' => 'Telefono numeris',
+            'type' => 'text',
+            'placeholder' => 'only digits...',
+            'value' => '',
+            'filter' => FILTER_SANITIZE_NUMBER_INT,
+        ],
+    ],
+    'callbacks' => [
+        'success' => 'form_success',
+        'fail' => 'form_fail'
+    ]
+];
+
+$filtered_input = get_form_input($form);
+$th_foreach = [];
+
+if ($filtered_input) {
+    validate_form($filtered_input, $form);
+    $th_foreach = show_th($form);
 }
 
 $data = file_to_array('info.txt');
 if (!$data) {
     $data = [];
+};
+
+function show_th($formos_array) {
+    $th_array = [];
+    foreach($formos_array['fields'] as $field_id => $field){
+        $th_array[] = $field['label'];
+    }
+    return $th_array;
 }
-
-
 
 ?>
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>06.25</title>
-    <link rel="stylesheet" type="text/css" href="include/normalise.css">
-    <link rel="stylesheet" type="text/css" href="include/style.css">
+    <title>PHP form</title>
+    <link rel="stylesheet" type="text/css" href="css/normalise.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
 <form action="<?php print $form['action']; ?>" method="<?php print $form['method']; ?>">
-    <?php foreach ($form['fields'] as $field_id => $field): ?>
-        <label><?php print $field['label']; ?></label>
-        <input type="<?php print $field['type']; ?>"
-               name="<?php print $field_id; ?>"
-            <?php if (isset($field['value'])): ?>
-                value="<?php print $field['value']; ?>"
-            <?php endif; ?>
-            <?php if (isset($field['placeholder'])): ?>
-                value="<?php print $field['placeholder']; ?>"
-            <?php endif; ?>
-        >
-        <?php if (isset($field['error'])): ?>
-            <span class="error"><?php print $field['error']; ?></span>
+    <?php foreach ($form['fields'] as $key => $input): ?>
+        <?php if (isset($input['label'])): ?>
+            <label for="<?php print $key; ?>"><?php print $input['label']; ?></label>
+        <?php endif; ?>
+
+        <input name="<?php print $key; ?>"
+               type="<?php print $input['type']; ?>"
+               placeholder="<?php print $input['placeholder']; ?>"
+               value="<?php print $input['value']; ?>">
+        <?php if (isset($input['error'])): ?>
+            <span class="error"><?php print $input['error']; ?></span>
         <?php endif; ?>
     <?php endforeach; ?>
-    <button name="mygtukas">Siusti</button>
+    <button>Siusti</button>
 </form>
 <table>
     <tr>
-        <th>Vardas</th>
-        <th>Pavarde</th>
-        <th>Amzius</th>
+        <?php foreach ($th_foreach as $th): ?>
+            <th><?php print $th; ?></th>
+        <?php endforeach; ?>
     </tr>
     <?php foreach ($data as $row): ?>
         <tr>
-            <?php foreach ($row as $td_column): ?>
-                <td><?php print $td_column; ?></td>
+            <?php foreach ($row as $column): ?>
+                <td>
+                    <?php print $column; ?>
+                </td>
             <?php endforeach; ?>
         </tr>
     <?php endforeach; ?>
